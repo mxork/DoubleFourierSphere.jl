2616d6a875d993a9e08579e14d94533fe43f9b8e
Dan Anderson <dan.hillier.anderson@gmail.com>
2017-05-23 16:27:43 -0400

    copy pasta'd unfast dz

--- vorticity.jl
+++ vorticity.jl
@@ -5,7 +5,61 @@ export dζ, plan_dζf!, courant_number
 # Z = vorticity grid
 # F = coriolis forcing
 function dζ(Z, F)
-    # TODO splice back in the naive version from old git
+    # some helper cruft
+    M = Int(round(size(Z,1)/2))
+    Ms = [0:M-1 ; -M:-1]
+
+    N = size(Z,2)
+    Ns0 = 0:N-1
+    Ns = 1:N
+
+    # the meat begins
+    Zf = fft_sphere(Z)
+    Ff = fft_sphere(F) # Terrible notation, but consistent
+    Hf = Zf + Ff
+    Ψf = laplace_inv_spectral(Zf)
+
+    # Vf = im*Ψf
+    Vf = 1.0im*diagm(Ms)*Ψf
+
+    Uf = zeros(Vf)
+    A = sinφdφ(N)
+    B = sinφdφ_even_not_zero(N)
+    for mi in 1:size(Ψf, 1)
+        m = Ms[mi]
+        Uf[mi, :] = iseven(m) && m != 0 ?
+                    B * Ψf[mi,:] :
+                    A * Ψf[mi,:]
+    end
+
+    # similar for η derivatives
+    Hλf = 1.0im*diagm(Ms)*Hf
+    Hφf = zeros(Hλf)
+    for mi in 1:size(Hf, 1)
+        m = Ms[mi]
+        # extra -ve sign intentional; see defs
+        # otherwise, same matrices
+        Hφf[mi, :] = iseven(m) && m != 0 ?
+                    B * Hf[mi,:] :
+                    A * Hf[mi,:]
+    end
+
+    # now we have the derived quantites, pop back
+    # into normal space and do the PW multiplication
+    # these can be FASTER
+    U, V = ifft_sphere(Uf), ifft_sphere(Vf)
+    Hλ, Hφ = ifft_sphere(Hλf), ifft_sphere(Hφf)
+
+    # §3-8, Cheong
+    Φs = Complex128[ π*(j+0.5)/N for j in 0:N-1]
+    S = diagm( 1 ./ sin(Φs) )
+    X = (U .* Hλ) *S*S
+    Y = (V .* Hφ) *S
+
+    Xf, Yf = fft_sphere(X), fft_sphere(Y)
+
+    # and here is dζ/dt (spectral, actually)
+    ifft_sphere(-Xf - Yf)
 end
 
 # plans a computation of the instantenous dζ_{m,n}/dt given a
